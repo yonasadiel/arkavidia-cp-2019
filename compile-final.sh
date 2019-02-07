6cd8061260df8e9be94cca196a31a9b8183eb44a
Yonas Adiel <yonazadielwiguna@gmail.com>
2019-02-08 05:39:05 +0700

    revision after proofread by radit

---
+++ compile-final.sh
@@ -0,0 +1,101 @@
+cd arkavidia-2019-final-arvymenjemurpakaian
+pdflatex statement.tex
+rm *.aux
+rm *.log
+rm *.dvi
+rm *.out
+cd ..
+
+cd arkavidia-2019-final-barisanyangindah
+pdflatex statement.tex
+rm *.aux
+rm *.lohflog
+rm *.dvi
+rm *.out
+cd ..
+
+cd arkavidia-2019-final-cincinarvy
+pdflatex statement.tex
+rm *.aux
+rm *.lohflog
+rm *.dvi
+rm *.out
+cd ..
+
+cd arkavidia-2019-final-danauganesha
+pdflatex statement.tex
+rm *.aux
+rm *.lohflog
+rm *.dvi
+rm *.out
+cd ..
+
+cd arkavidia-2019-final-ekspansidanauganesha
+pdflatex statement.tex
+rm *.aux
+rm *.lohflog
+rm *.dvi
+rm *.out
+cd ..
+
+cd arkavidia-2019-final-fungsibilanganacak
+pdflatex statement.tex
+rm *.aux
+rm *.lohflog
+rm *.dvi
+rm *.out
+cd ..
+
+cd arkavidia-2019-final-gudangmagnetik
+pdflatex statement.tex
+rm *.aux
+rm *.lohflog
+rm *.dvi
+rm *.out
+cd ..
+
+cd arkavidia-2019-final-haribekerja
+pdflatex statement.tex
+rm *.aux
+rm *.lohflog
+rm *.dvi
+rm *.out
+cd ..
+
+cd arkavidia-2019-final-investasirestoran
+pdflatex statement.tex
+rm *.aux
+rm *.lohflog
+rm *.dvi
+rm *.out
+cd ..
+
+cd arkavidia-2019-final-jalurkereta
+pdflatex statement.tex
+rm *.aux
+rm *.lohflog
+rm *.dvi
+rm *.out
+cd ..
+
+cd arkavidia-2019-final-kubusrubik
+pdflatex statement.tex
+rm *.aux
+rm *.lohflog
+rm *.dvi
+rm *.out
+cd ..
+
+
+pdftk arkavidia-2019-final-arvymenjemurpakaian/statement.pdf \
+    arkavidia-2019-final-barisanyangindah/statement.pdf \
+    arkavidia-2019-final-cincinarvy/statement.pdf \
+    arkavidia-2019-final-danauganesha/statement.pdf \
+    arkavidia-2019-final-ekspansidanauganesha/statement.pdf \
+    arkavidia-2019-final-fungsibilanganacak/statement.pdf \
+    arkavidia-2019-final-gudangmagnetik/statement.pdf \
+    arkavidia-2019-final-haribekerja/statement.pdf \
+    arkavidia-2019-final-investasirestoran/statement.pdf \
+    arkavidia-2019-final-jalurkereta/statement.pdf \
+    arkavidia-2019-final-kubusrubik/statement.pdf \
+    cat output problems.pdf
