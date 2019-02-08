0fdec7241ae3b74317df2c8e4735a01425a302f6
Tony <buddy90_lost@yahoo.co.id>
2019-02-09 02:35:24 +0700

    fix on solution-alt-2 problem final c

--- arkavidia-2019-final-cincinarvy/solution-alt-2.cpp
+++ arkavidia-2019-final-cincinarvy/solution-alt-2.cpp
@@ -17,7 +17,7 @@ int solve(){
     __uint128_t bd = abs(d);
     __uint128_t bx = abs(x);
     // if(va > vk || (1.0L*(y+d)*vk/va <= sqrtl(1LL*x*x + 1LL*(y+d)*(y+d)) && (1.0L*(y)*vk/va <= sqrtl(1LL*x*x + 1LL*(y)*(y)))))
-    if(va > vk || (p2(by+bd)*p2(bvk) <= p2(bva)*(p2(bx) + p2(by+bd)) && p2(by)*p2(bvk) <= p2(bva)*(p2(y) + p2(x))))
+    if(va > vk || (p2(by+bd)*p2(bvk) < p2(bva)*(p2(bx) + p2(by+bd)) && p2(by)*p2(bvk) < p2(bva)*(p2(y) + p2(x))))
         puts("YA");
     else
         puts("TIDAK");
